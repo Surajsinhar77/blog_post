67414929f8c6f13a1175849487aa0d04ffa3cdf0
Suraj Kumar <surajsinhaking77@gmail.com>
2023-03-11 20:47:01 +0530

    all file push to git hub

---
+++ index.js
@@ -0,0 +1,7 @@
+const blog_card = document.querySelector('.blog-cards');
+
+console.log(blog_card);
+
+blog_card.addEventListener('click', ()=>{
+    window.location.href ="http://127.0.0.1:5500/blogContent.html";
+})
