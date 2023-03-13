7ae3ffe2c7d42a40726d7c027c91030290a42d9e
Suraj Kumar <surajsinhaking77@gmail.com>
2023-03-13 20:10:24 +0530

    update all

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log(blog_card);
 blog_card.addEventListener('click', ()=>{
     window.location.href ="http://127.0.0.1:5500/blogContent.html";
 })
+
+
+// git clone https://github.com/Surajsinhar77/blog_post.git
