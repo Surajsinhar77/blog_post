a9009bc9ec6005c18c2b0e89e82bce8bf0d46eef
Suraj Kumar <surajsinhaking77@gmail.com>
2023-03-30 21:18:01 +0530

    project Done

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from cryptography.fernet import Fernet
+from werkzeug.utils import secure_filename
+from datetime import datetime;
+import os
+
 
 
 app = Flask(__name__)
@@ -9,7 +13,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = 'qwertyuixcvbnm'
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/two_database'
+app.config['UPLOAD_FOLDER'] = "C:\\Users\\suraj\\OneDrive\Documents\\Development\project\\blog_post\static\\img"
+
+image = os.path.join('/static/', 'img');
 
 key =  Fernet.generate_key()
 fernet = Fernet(key) 
@@ -25,7 +33,7 @@ class Users(db.Model):
 
 
 class Blogs(db.Model):
-    s_no = db.Column(db.Integer, nullable = False, primary_key = True)
+    s_no = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(20), nullable = False)
     img1 = db.Column(db.String(30), nullable = False)
     img2 = db.Column(db.String(30), nullable = False)
@@ -34,13 +42,14 @@ class Blogs(db.Model):
     author_name = db.Column(db.String(30), nullable = False)
     date_time = db.Column(db.DateTime, nullable = False)
     content = db.Column(db.String(300), nullable = False)
+    Blog_type = db.Column(db.String(30), nullable =False)
 
 
 @app.route('/')
 def index():
     if('username' not in session ):
         blog_items = Blogs.query.all()
-        return render_template('index.html', blog_items = blog_items, session = session, user = 'username')
+        return render_template('index.html', blog_items = blog_items, session = session, user = 'username', image=image)
     return redirect(url_for('home', email = session['username']))
 
 
@@ -50,7 +59,7 @@ def home(email):
     if (session['username'] == email):
         # print(fernet.encrypt(email.encode()))
         blog_items = Blogs.query.all()
-        return render_template('index.html', blog_items = blog_items, email = email , user = 'username' ,session = session)
+        return render_template('index.html', blog_items = blog_items, email = email , user = 'username' ,session = session, image=image)
     return redirect(url_for('login'))
 
 @app.route('/login')
@@ -81,7 +90,7 @@ def login_auth():
         # print("email from html form "+email)
         check = Users.query.filter_by(email = email).first()
         if(check is None):
-            return "Invalid input"
+            flash("Invalid input")
         else:
             if(check.password == password):
                 session['username'] = email
@@ -91,7 +100,7 @@ def login_auth():
 
 @app.route('/log_out')
 def logOut():
-    session.pop('username', None)
+    session.pop('username' , None)
     return redirect(url_for('login'))
 
 @app.route('/signup')
@@ -99,10 +108,11 @@ def sign():
     return render_template('signup.html')
 
 
-@app.route('/blog-content/<email>')
-def blog_content(email):
-    if(session['username'] == email):
-        return render_template('blogContent.html' , email = email)
+@app.route('/blog-content/', methods=['GET','POST'])
+def blog_content():
+    if('username' in session):
+        ContentData  =  Blogs.query.filter_by( title = request.args.get('title')).all();
+        return render_template('blogContent.html', email = session['username'] ,user = 'username', session = session, contentData = ContentData, image = image)
     else:
         return redirect(url_for('login'))
 
@@ -110,12 +120,36 @@ def blog_content(email):
 def handelCreateBlogAtLogout():
     return redirect(url_for('login'))
 
+@app.route('/submitBlog' , methods = ['GET', 'POST'])
+def submittingBlog():
+    if 'username' in session:
+        if request.method == 'POST':
+            title = request.form.get('title')
+            anytorname = request.form.get('authorName')
+            typ = request.form.get('contentType')
+            dateAndTime = request.form.get('dateAndTime')
+            print(dateAndTime)
+            content = request.form.get('Content')
+            img1head = request.form.get('imgSubhead1')
+            img1head2 = request.form.get('imgSubhead2')
+            fl = request.files['file1']
+            fl2 = request.files['file2']
+            date__time = datetime.now();
+            enrty = Blogs(title = title, img1 = fl.filename, img2 = fl2.filename, author_name = anytorname, date_time = date__time, content = content, Blog_type = typ, img1sub = img1head, img2sub = img1head2)
+            fl.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(fl.filename)))
+            fl2.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(fl2.filename)))
+            db.session.add(enrty)
+            db.session.commit()
+            flash('Submit Successfull')
+            flash("SuccessFull Uploaderd")
+        return redirect(url_for("create_blog", email = session['username']))
+    return redirect(url_for('login'))
+
+
 @app.route('/create-blog/<email>')
 def create_blog(email):
-    if('username'in session):
-        print('in the create blog if ')
+    if('username' in session):
         return render_template("blogForm.html", email = email, user= 'username', session = session)
-    print("check here ")
     return redirect(url_for('login'))
 
 if __name__=="__main__":
--- static/index.js
+++ static/index.js
@@ -1,10 +1,14 @@
-const blog_card = document.querySelector('.blog-cards');
+const blog_card = document.querySelectorAll('.blog-cards');
 
+// let a = 1;
+blog_card.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        let title_data = item.querySelector('.card-title').textContent;
+        window.location.href = `/blog-content?title=${title_data}`;
+    })
 
-
-blog_card.addEventListener('click', ()=>{
-    window.location.href ="/blog-content";
 })
+
 
 function ellipsify(str) {
     if (str.length > 10) {
@@ -21,6 +25,15 @@ cardTextAll.forEach(element => {
     element.innerHTML = ellipsify(element.textContent);
 });
 
+let getMsg = document.querySelector('.error_msg');
+let getMsgchange = document.querySelector('.error_msg').textContent;
+
+getMsg.addEventListener('change',()=>{
+    setTimeout(()=>{
+        console.log("can u come here")
+        getMsg.style.display = 'none';
+    },2000)
+})
 
 
 
